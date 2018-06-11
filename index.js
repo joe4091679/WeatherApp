f83435ae1e0eb8c3d24afd3b4fbc6901f841b6ef
呂雨樵 <joe@Macbook-Pro-6.local>
2018-06-12 01:12:41 +0800

    location

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require("express"),
-	app = express(),
-	request = require("request"),
-	bodyParser = require("body-parser"),
-	flash = require("connect-flash"),
-	google_key = process.env.GEOCODER_KEY,
-	weather_key = process.env.WEATHER_KEY;
+		app = express(),
+		request = require("request"),
+		bodyParser = require("body-parser"),
+		flash = require("connect-flash"),
+		getmac = require("getmac"),
+		google_key = process.env.GEOCODER_KEY || "AIzaSyCqAG89ckLgquX7gbXkJ6wQjT37R8k7iE8",
+		weather_key = process.env.WEATHER_KEY || "fad2c02707d9526deef6c69ef62d538e",
+		getPhotoReferenceByLocationURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + google_key + "&location=",
+		getPhotoReferenceByPlaceIdURL = "https://maps.googleapis.com/maps/api/place/details/json?key=" + google_key + "&placeid=",
+		getPhotosByPhotoReferenceURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&key=" + google_key + "&photoreference=",
+		getWeatherInformationURL = "http://api.openweathermap.org/data/2.5/weather?appid=" + weather_key + "&lat=",
+		geolocation = require("google-geolocation")({key: google_key});
 var NodeGeocoder = require('node-geocoder');
 
 var options = {
@@ -15,6 +21,7 @@ var options = {
 };
 
 var geocoder = NodeGeocoder(options);
+
 //======================================================================
 //						CHECK LIST
 //======================================================================
@@ -33,30 +40,56 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(flash());
 app.set("view engine", "ejs");
+// middleware
 app.use(function (req, res, next) {
+		res.locals.language = "English";
   	res.locals.url = [];
   	res.locals.isInLanding = false;
   	res.locals.error = req.flash("error");
   	next();
 })
 //======================================================================
-//							INDEX ROUTE
+//							INDEX ROUTE (Landing page)
 //======================================================================
 app.get("/", function(req, res){
-	res.locals.isInLanding = true;
-	res.render("landing");
+	res.render("landing", {isInLanding: true});
 });
 
-app.post("/", function(req, res){
-	var city = req.body.city;
-	res.redirect("/"+city);
-});
 
 //======================================================================
 //							SHOW ROUTE
 //======================================================================
-app.get("/:city", function(req, res){
-	var city = req.params.city;
+// show the weather condition of current user's position
+app.get("/myLocation", function(req, res){
+	// locate user using MAC address
+	geolocation(getMacAddress(), function(err, data){
+		if (err) {
+			req.flash("error", "Error! Can't find you by your MAC address!");
+			res.redirect("back");
+		} else {
+			var lat = data.location.lat;
+			var lng = data.location.lng;
+			var location = [lat, lng];
+			// get photo_reference of locations by location
+			request(getPhotoReferenceByLocationURL + location + "&radius=100", function(error, response, body){
+				if (err) {
+					req.flash("error", "Error! Can't get photos by location!");
+					res.redirect("back");
+				} else {
+					var jsonData = JSON.parse(body);
+					var country = "";
+					var city = jsonData.results[0].name;
+					getPhotos(jsonData, "myLocation", res);
+					getWeatherInformation(lat, lng, country, city, res);
+				}
+			});
+		}
+	})
+});
+
+app.get("/city", function(req, res){
+	var city = req.query.city;
+	// eval(require("locus"));
 	geocoder.geocode(city, function (err, data) {
 		if (err || !data.length) {
 			req.flash('error', 'Invalid address');
@@ -66,60 +99,19 @@ app.get("/:city", function(req, res){
 		var lng = data[0].longitude;
 		var location = data[0].formattedAddress;
 		var placeId = data[0].extra.googlePlaceId;
-		// get photos of locations
-		request("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + placeId + "&key=" + google_key, function(error, response, body){
+		// get photos_reference of locations by placeId
+		request(getPhotoReferenceByPlaceIdURL + placeId, function(error, response, body){
 			if (!error && response.statusCode == 200) {
 				// parse the jsonData to get the required place reference
 				var jsonData = JSON.parse(body);
-				console.log(jsonData);
+				// console.log(jsonData);
 				if (jsonData.status === 'OK') {
 					var country = jsonData.result.address_components.slice(-1)[0].short_name;
-					if (typeof jsonData.result.photos !== 'undefined') {
-						var photoNum = jsonData.result.photos.length;
-						var reference = [];
-
-						// get all the background images url
-						for (var i = 0; i < photoNum; i++) {
-							reference.push(jsonData.result.photos[i].photo_reference);
-							res.locals.url.push("https://maps.googleapis.com/maps/api/place/photo?maxwidth=2000&photoreference=" + reference[i] + "&key=" + google_key);
-						}
-					}
-					// get weather information
-					request("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + weather_key, function(error, response, body){
-						if (!error && response.statusCode == 200) {
-							// parse the jsonData to get the required weather information
-							var jsonData = JSON.parse(body);
-							var temp = k2c(jsonData.main.temp);
-							var weather = jsonData.weather[0];
-							var sunrise = new Date(jsonData.sys.sunrise*1000);
-							var sunset = new Date(jsonData.sys.sunset*1000);
-							var sunriseTime = {
-								hours: preZero(sunrise.getHours()),
-								minutes: preZero(sunrise.getMinutes()),
-								seconds: preZero(sunrise.getSeconds())
-							}
-							var sunsetTime = {
-								hours: preZero(sunset.getHours()),
-								minutes: preZero(sunset.getMinutes()),
-								seconds: preZero(sunset.getSeconds())
-							}
-							res.render("show", {
-								country: country,
-								city: city,
-								lat: lat,
-								lng: lng,
-								temp: temp,
-								weather: weather,
-								sunriseTime: sunriseTime,
-								sunsetTime: sunsetTime
-							});
-						} else {
-							req.flash("error", body.error_message);
-							res.redirect("/");
-						} // get weather information
-					});
+					getPhotos(jsonData, "city", res);
+					getWeatherInformation(lat, lng, country, city, res);
 				} else {
 					req.flash("error", jsonData.status);
+					res.redirect("back");
 				} // status is not 'OK'
 			} else {
 				req.flash("error", body.error_message);
@@ -131,16 +123,95 @@ app.get("/:city", function(req, res){
 
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-// app.listen("3000", function(){
+// app.listen(process.env.PORT, process.env.IP, function(){
+app.listen("3000", function(){
 	console.log("SERVER HAS STARTED!!!");
 });
 
+// get photos of location
+function getPhotos(jsonData, route, res){
+	var photoNum;
+	switch (route) {
+		case "city":
+			if (typeof jsonData.result.photos !== 'undefined') {
+				photoNum = jsonData.result.photos.length;
+			}
+			break;
+		case "myLocation":
+			photoNum = jsonData.results.length;
+			break;
+		default:
+			break;
+	}
+
+
+	// get all the background images url
+	for (var i = 0; i < photoNum; i++) {
+		switch (route) {
+			case "city":
+				res.locals.url.push(getPhotosByPhotoReferenceURL + jsonData.result.photos[i].photo_reference);
+				break;
+			case "myLocation":
+				if (typeof jsonData.results[i].photos !== 'undefined') {
+					res.locals.url.push(getPhotosByPhotoReferenceURL + jsonData.results[i].photos[0].photo_reference);
+				}
+				break;
+			default:
+				break;
+		}
+	}
+}
+// get Mac address to position the user
+function getMacAddress(req){
+	getmac.getMac(function(err, mac){
+		if (err) {
+			req.flash("error", "Error! Can't get MAC address!");
+		} else {
+			return {macAddress: mac};
+		}
+	});
+}
+// get weather information with openWeathermap API
+function getWeatherInformation(lat, lng, country, city, res){
+	// get weather information
+	request(getWeatherInformationURL + lat + "&lon=" + lng, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			// parse the jsonData to get the required weather information
+			var jsonData = JSON.parse(body);
+			var temp = k2c(jsonData.main.temp);
+			var weather = jsonData.weather[0];
+			var sunrise = new Date(jsonData.sys.sunrise*1000);
+			var sunset = new Date(jsonData.sys.sunset*1000);
+			var sunriseTime = {
+				hours: preZero(sunrise.getHours()),
+				minutes: preZero(sunrise.getMinutes()),
+				seconds: preZero(sunrise.getSeconds())
+			}
+			var sunsetTime = {
+				hours: preZero(sunset.getHours()),
+				minutes: preZero(sunset.getMinutes()),
+				seconds: preZero(sunset.getSeconds())
+			}
+			res.render("show", {
+				country: country,
+				city: city,
+				lat: lat,
+				lng: lng,
+				temp: temp,
+				weather: weather,
+				sunriseTime: sunriseTime,
+				sunsetTime: sunsetTime
+			});
+		} else {
+			req.flash("error", body.error_message);
+			res.redirect("/");
+		} // get weather information
+	});
+}
 // Kelvin to Celsius conversion
 function k2c(k){
 	return parseFloat(k - 272.15).toFixed(0);
 }
-
 // prepend a zero to a number and return 2 LSB
 function preZero(num) {
 	return ("0" + num).slice(-2);
--- public/asset/js/show.js
+++ public/asset/js/show.js
@@ -1,12 +1,34 @@
-$("input:checkbox").on("click", function(){
-  var str = $("#temp").text();
-  var pos = str.indexOf("°");
-  var temp = parseInt(str.slice(0, pos-1));
-  if ($(this).is(":checked")) {
-    temp = (temp - 32) * 5 / 9;
-    $("#temp").text(temp.toFixed(0) + " °C");
-  } else {
-    temp = temp * 9 / 5 + 32;
-    $("#temp").text(temp.toFixed(0) + " °F");
-  }
+$(function(){
+  $("input:checkbox").on("click", function(){
+    var str = $("#temp").text();
+    var pos = str.indexOf("°");
+    var temp = parseInt(str.slice(0, pos-1));
+    if ($(this).is(":checked")) {
+      temp = (temp - 32) * 5 / 9;
+      $("#temp").text(temp.toFixed(0) + " °C");
+    } else {
+      temp = temp * 9 / 5 + 32;
+      $("#temp").text(temp.toFixed(0) + " °F");
+    }
+  });
+
+  $("#chinese").on("click", function(){
+    language = "Chinese";
+    $("#language").text("語言");
+    $("#search").text("搜尋");
+    $("#searchInput").attr("placeholder", "搜尋");
+    $("#sunrise").text("日出");
+    $("#sunset").text("日落");
+    $("#temperature").text("溫度");
+  });
+
+  $("#vietnamese").on("click", function(){
+    language = "Vietnamese";
+    $("#language").text("Ngôn Ngữ");
+    $("#search").text("Tìm Kiếm");
+    $("#searchInput").attr("placeholder", "Tìm Kiếm");
+    $("#sunrise").text("Mặt Trời Mọc");
+    $("#sunset").text("Mặt Trời Lặn");
+    $("#temperature").text("Nhiệt độ");
+  });
 });
